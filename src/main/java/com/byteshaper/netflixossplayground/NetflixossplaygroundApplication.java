52f6b5a959c8f6c0b1bcb004249a58c8d6271f22
Henning Schuetz <henning.schuetz@zanox.com>
2016-10-13 11:56:52 +0200

    basic project structure

---
+++ src/main/java/com/byteshaper/netflixossplayground/NetflixossplaygroundApplication.java
@@ -0,0 +1,12 @@
+package com.byteshaper.netflixossplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixossplaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixossplaygroundApplication.class, args);
+	}
+}
